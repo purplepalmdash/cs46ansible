8876ba16a392b69041ff9e292713c701b5652d75
feipyang <feipyang@gmail.com>
2015-12-24 21:21:27 +0800

    Added in Dec24

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@ sudo ansible-playbook -i inventory1 network.yml  -k
 sudo ansible-playbook -i inventory combine.yml -k
 sudo ansible-playbook -i inventory cloudmonkey.yml -k
 
+# 4.5.1
+sudo ansible-playbook -i inventory1 network.yml -k
+sudo ansible-playbook -i inventory45 cloudstack451.yml -k
+sudo ansible-playbook -i inventory45 cloudmonkey.yml -k
